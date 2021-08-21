7143b07ea8587b621fd86778646b2fce2a7d39d3
Gary Ascuy <gary.ascuy@gmail.com>
2021-08-21 11:19:36 -0500

    Updating

--- site/pages/api/pass.ts
+++ site/pages/api/pass.ts
@@ -2,10 +2,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import fs from 'fs'
 
-type Data = {
-  name: string
-}
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
