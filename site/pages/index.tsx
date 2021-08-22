b41ac5f4ef2abd409b9821c4d4f780253cb16ccb
Gary Ascuy <gary.ascuy@gmail.com>
2021-08-22 09:10:23 -0500

    Refactoring pages

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,7 +1,7 @@
-/* eslint-disable @next/next/no-html-link-for-pages */
-
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
+
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -17,7 +17,8 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to Certi Covid
         </h1>
-        <a href="/api/pass">DOWNLOAD</a>
+
+        <Link href='/pass/scan'>SCAN</Link>
       </main>
 
       <footer className={styles.footer}>
