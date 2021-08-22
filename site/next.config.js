4d378ce261470ce3e92ef330adadaf01cc37bd81
Gary Ascuy <gary.ascuy@gmail.com>
2021-08-21 22:27:37 -0500

    Removing unnecessary vars

--- site/next.config.js
+++ site/next.config.js
@@ -2,8 +2,4 @@
 
 module.exports = {
   reactStrictMode: true,
-  env: {
-    COVID__PASS_TEMPLATE_URL: 'http://localhost:3666/templates/pass/template.pass',
-    COVID__SIGNER_CERT_PASSWORD: 'gary',
-  }
 }
