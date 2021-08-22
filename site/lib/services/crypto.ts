eee42d8b4d1c36fd71b590fa43ff432f85a7d8b9
Gary Ascuy <gary.ascuy@gmail.com>
2021-08-21 22:28:08 -0500

    Adding crypto module

---
+++ site/lib/services/crypto.ts
@@ -0,0 +1,22 @@
+import crypto from 'crypto'
+
+const algorithm = 'aes-256-cbc'
+const secretKey = process.env.COVID__CRYPTO_SECRET_KEY || ''
+const iv = Buffer.from(process.env.COVID__CRYPTO_IV || '', 'hex')
+
+export function encrypt(message: string): string {
+  const cipher = crypto.createCipheriv(algorithm, secretKey, iv)
+  const encrypted = Buffer.concat([cipher.update(message), cipher.final()]);
+  return encrypted.toString('hex');
+}
+
+export function decrypt(hash: string): string {
+  const decipher = crypto.createDecipheriv(algorithm, secretKey, iv);
+  const decrpyted = Buffer.concat([decipher.update(Buffer.from(hash, 'hex')), decipher.final()]);
+  return decrpyted.toString();
+}
+
+export function getRandomIV(): string {
+  const iv = crypto.randomBytes(16)
+  return iv.toString('hex')
+}
