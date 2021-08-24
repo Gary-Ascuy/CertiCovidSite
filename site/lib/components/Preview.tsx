2b6580d52f3a0e68351d26afd804dbf75ef57283
Gary Ascuy <gary.ascuy@gmail.com>
2021-08-23 23:24:10 -0500

    Refactoring into components

---
+++ site/lib/components/Preview.tsx
@@ -0,0 +1,35 @@
+import { VaccinationInformation } from '../models/VaccinationInformation'
+
+export interface PreviewProps {
+  person: VaccinationInformation
+}
+
+export default function Preview({ person }: PreviewProps) {
+  const { name, ci, birthday, municipality, vaccinationDate } = person
+  return (
+    <div className='grid gap-4 grid-cols-2'>
+      <div className='col-span-2'>
+        <div className='text-gray-400 text-sm'>Nombres y Apellidos</div>
+        <div className='text-black font-medium text-lg'>{name}</div>
+      </div>
+
+      <div>
+        <div className='text-gray-400 text-xs'>Nro. Documento:</div>
+        <div className='text-black font-medium text-sm'>{ci}</div>
+      </div>
+      <div>
+        <div className='text-gray-400 text-xs'>Fecha de Nacimiento:</div>
+        <div className='text-black font-medium text-sm'>{birthday}</div>
+      </div>
+
+      <div>
+        <div className='text-gray-400 text-xs'>Municipio:</div>
+        <div className='text-black font-medium text-sm'>{municipality}</div>
+      </div>
+      <div>
+        <div className='text-gray-400 text-xs'>Fecha de Vacunación:</div>
+        <div className='text-black font-medium text-sm'>{vaccinationDate}</div>
+      </div>
+    </div>
+  )
+}
---
+++ site/lib/components/Step.tsx
@@ -0,0 +1,23 @@
+export interface StepProps {
+  step: string
+  title: string
+  enabled?: boolean
+  children: JSX.Element
+}
+
+export default function Step({ step, title, children, enabled = true }: StepProps) {
+  return (
+    <div className={`flex flex-col space-y-5 ${enabled ? '' : 'opacity-50'}`}>
+      <div className='rounded-md p-6 bg-gray-100 space-y-4'>
+        <div className='flex flex-row items-center'>
+          <div className="rounded-md p-4 bg-blue-400 h-5 w-5 flex items-center justify-center">
+            <p className="text-white text-lg font-bold">{step}</p>
+          </div>
+          <div className="ml-3 font-bold text-xl text-primary">{title}</div>
+        </div>
+
+        <div className='text-lg'> {children} </div>
+      </div>
+    </div>
+  )
+}
