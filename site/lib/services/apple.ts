0003dfde9a13bc97b755f80363f049ca23890da5
Gary Ascuy <gary.ascuy@gmail.com>
2021-08-24 19:34:04 -0500

    Improving generation apple wallet certs

--- site/lib/services/apple.ts
+++ site/lib/services/apple.ts
@@ -7,23 +7,26 @@ import { VaccinationInformation } from "../models/VaccinationInformation"
 const format = 'PKBarcodeFormatQR'
 const messageEncoding = 'iso-8859-1'
 
-export function buildApplePass(urlBase64: string, personalData: ResponsePayload<VaccinationInformation>): Partial<ApplePass> {
+export function buildApplePass(urlBase64: string, response: ResponsePayload<VaccinationInformation>): Partial<ApplePass> {
+  const vaccine = response.data
+
   // QR Code
   const url = Buffer.from(urlBase64, 'base64').toString()
   const barcode = { format, message: url, messageEncoding, altText: 'CertiCovid' } as BarcodeDescriptor
 
   // Primary Fields
-  const name = { key: 'name', label: 'NOMBRE', value: personalData.data.name }
+  const name = { key: 'name', label: 'NOMBRE', value: vaccine.name }
 
   // Secondary Fields
-  const supplier = { key: 'supplier', label: 'PROVEEDOR', value: personalData.data.supplier }
-  const dose = { key: 'dose', label: 'DOSIS', value: personalData.data.dose }
-  const vaccinationDate = { key: 'vaccinationDate', label: 'FECHA', value: personalData.data.vaccinationDate }
+  const supplier = { key: 'supplier', label: 'PROVEEDOR', value: vaccine.supplier }
+  const dose = { key: 'dose', label: 'DOSIS', value: vaccine.dose }
+  const vaccinationDate = { key: 'vaccinationDate', label: 'FECHA', value: vaccine.vaccinationDate }
 
   // Back Fields
-  const personal = { key: 'personal', label: 'Información Personal', value: `Nombre completo:\n${personalData.data.name}\nDocumento de Identidad:\n${personalData.data.ci}\nFecha nacimiento:\n${personalData.data.birthday}` }
-  const place = { key: 'place', label: 'Lugar de Vacunación', value: `Departamento:\n${personalData.data.departament}\nMunicipio:\n${personalData.data.municipality}\nEstablecimiento:\n${personalData.data.establishment}` }
-  const vaccine = { key: 'vaccine', label: 'Información de la Vacuna', value: `Fecha vacunacion:\n${personalData.data.vaccinationDate}\nVacuna:\n${personalData.data.vaccine}\nDosis:\n${personalData.data.dose}\nProveedor:\n${personalData.data.supplier}\nLote:\n${personalData.data.lot}\nNro. consentimiento:\n${personalData.data.consentNumber}\nFecha proxima vacunacion:\n${personalData.data.nextVaccinationDate}\n` }
+  const personal = { key: 'personal', label: 'Información Personal', value: `Nombre completo:\n${vaccine.name}\nDocumento de Identidad:\n${vaccine.ci}\nFecha nacimiento:\n${vaccine.birthday}` }
+  const place = { key: 'place', label: 'Lugar de Vacunación', value: `Departamento:\n${vaccine.departament}\nMunicipio:\n${vaccine.municipality}\nEstablecimiento:\n${vaccine.establishment}` }
+  const vaccineDetails = { key: 'vaccine', label: 'Información de la Vacuna', value: `Fecha vacunacion:\n${vaccine.vaccinationDate}\nVacuna:\n${vaccine.vaccine}\nDosis:\n${vaccine.dose}\nProveedor:\n${vaccine.supplier}\nLote:\n${vaccine.lot}\nNro. consentimiento:\n${vaccine.consentNumber}\nFecha proxima vacunacion:\n${vaccine.nextVaccinationDate || 'Vacuna al día'}` }
+  const createdAt = { key: 'createdAt', label: 'Fecha de Generación', value: new Date().toUTCString() }
 
   const terms = { key: 'TERMS', label: 'Terminos del servicio', value: 'Tenga en cuenta que no somos los emisores de los certificados originales y no estamos conectados de ninguna manera con las autoridades oficiales del País ni los representamos de ninguna manera. Estamos transfiriendo el mismo código QR de validación de su certificado original en papel/PDF al formato digital, y la funcionalidad se ha probado con todas las aplicaciones de validación disponibles en el momento del desarrollo.\n\nCertiCovid no reemplaza el Certificado emitido por el País, solo lo digitaliza. Le recomendamos encarecidamente que tenga a mano su certificado original y busque activamente la información y las medidas más recientes del uso del certificado en su país.' }
   const team = { key: 'TEAM', label: 'Equipo', value: '- Gary Ascuy @garyascuy\n- Rodrigo Ergueta @rodrigoergueta\n- Ramiro Ergueta @ramiroergueta' }
@@ -31,7 +34,7 @@ export function buildApplePass(urlBase64: string, personalData: ResponsePayload<
   const generic = {
     primaryFields: [name],
     secondaryFields: [supplier, dose, vaccinationDate],
-    backFields: [personal, place, vaccine, terms, team]
+    backFields: [personal, place, vaccineDetails, createdAt, terms, team],
   }
 
   // Base Fields
