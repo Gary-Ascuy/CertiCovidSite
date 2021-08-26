74a742733ded00050c2f10d613a27827aac02998
Gary Ascuy <gary.ascuy@gmail.com>
2021-08-25 20:26:34 -0500

    Updating header

--- site/lib/components/Header.tsx
+++ site/lib/components/Header.tsx
@@ -1,16 +1,24 @@
+import Image from 'next/image'
+
 export interface HeaderProps {
   title?: string
 }
 
-export default function Header({ title = 'CertiCovid Bolivia' }: HeaderProps) {
+export default function Header({ title = 'CertiCovid' }: HeaderProps) {
   return (
     <div className='flex flex-row items-center p-3 justify-center space-x-1'>
-      <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-shield-check" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#006C9D" fill="none" strokeLinecap="round" strokeLinejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-        <path d="M9 12l2 2l4 -4" />
-        <path d="M12 3a12 12 0 0 0 8.5 3a12 12 0 0 1 -8.5 15a12 12 0 0 1 -8.5 -15a12 12 0 0 0 8.5 -3" />
+      <svg xmlns='http://www.w3.org/2000/svg' className='icon icon-tabler icon-tabler-id' width='50' height='50' viewBox='0 0 24 24' strokeWidth='1.5' stroke='#006C9D' fill='none' strokeLinecap='round' strokeLinejoin='round'>
+        <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+        <rect x='3' y='4' width='18' height='16' rx='3' />
+        <circle cx='9' cy='10' r='2' />
+        <line x1='15' y1='8' x2='17' y2='8' />
+        <line x1='15' y1='12' x2='17' y2='12' />
+        <line x1='7' y1='16' x2='17' y2='16' />
       </svg>
-      <div className='text-3xl font-bold text-primary'>{title}</div>
+      &nbsp; &nbsp;
+      <div className='text-5xl font-bold text-primary'>{title}</div>
+      &nbsp; &nbsp;
+      <Image src='/assets/bolivia/flag.png' height={45} width={76} alt='download pdf button'></Image>
     </div>
   )
 }
