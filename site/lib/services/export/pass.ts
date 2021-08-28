e9bc34ba00a5fec0c35de2cc574f4e60b037cf01
Gary Ascuy <gary.ascuy@gmail.com>
2021-08-28 08:15:28 -0500

    Fixing download pass in mobile

--- site/lib/services/export/pass.ts
+++ site/lib/services/export/pass.ts
@@ -6,5 +6,7 @@ export async function exportToPass(url: string, vaccine: VaccinationInformation)
   ga.event({ action: 'download', params: { type: 'pkpass' } })
 
   const bytes = await fetch(url).then(res => res.arrayBuffer())
-  saveAs(new Blob([bytes]), `CertiCovid_${new Date().toISOString().replace(/\W/g, '_')}.pkpass`)
+  const pass = new Blob([bytes], { type: 'application/vnd.apple.pkpass' })
+  const fileName = `CertiCovid_${new Date().toISOString().replace(/\W/g, '_')}.pkpass`
+  saveAs(pass, fileName)
 }
