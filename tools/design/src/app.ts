359ed0a435211521e80223c4db139cd89c43645c
Gary Ascuy <gary.ascuy@gmail.com>
2021-08-29 12:30:12 -0500

    Updating app module

--- tools/design/src/app.ts
+++ tools/design/src/app.ts
@@ -1,5 +1,58 @@
-import fs from 'fs'
+import { Template } from '@walletpass/pass-js'
+import { ImageDensity, ImageType } from '@walletpass/pass-js/dist/lib/images'
+import { promises as fs } from 'fs'
+import 'dotenv/config'
 
-console.log(process.env.FILENAME, "GARY ASCUY")
+import { passTypeIdentifier, teamIdentifier, value as data } from './data'
 
-fs.writeFileSync(process.env.FILENAME as any, 'gary', { encoding: 'utf-8' })
+export async function createTemplate() {
+  console.log('Creating Template')
+  const template = new Template('generic', {
+    passTypeIdentifier,
+    teamIdentifier,
+    sharingProhibited: false,
+  })
+
+  await template.loadCertificate(process.env.COVID__CERT_PATH as string, process.env.COVID__SIGNER_CERT_PASSWORD as string)
+
+  console.log('Loading Images')
+  const files: { path: string, type: ImageType, density: ImageDensity }[] = [{
+    type: 'icon',
+    path: './assets/icon.png',
+    density: '1x'
+  }, {
+    type: 'logo',
+    path: './assets/logo.png',
+    density: '1x'
+  }, {
+    type: 'logo',
+    path: './assets/logo@2x.png',
+    density: '2x'
+  }, {
+    type: 'logo',
+    path: './assets/logo@3x.png',
+    density: '3x'
+  }]
+
+  for (const { type, path, density } of files) {
+    const buffer = await fs.readFile(path)
+    await template.images.add(type, buffer, density)
+  }
+
+  return template
+}
+
+export async function build() {
+  const template = await createTemplate()
+
+  const file = process.env.FILENAME as string
+  console.log(`Generating PASS file into: ${file}`)
+  const pass = template.createPass(data)
+  const buffer = await pass.asBuffer()
+
+  console.log('Saving file')
+  await fs.writeFile(file, buffer, { encoding: 'utf-8' })
+  console.log('COMPLETED!!!')
+}
+
+build()
