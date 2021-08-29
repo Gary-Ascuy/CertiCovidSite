126397b64a9fef434419c697e7dd52158d26b765
Gary Ascuy <gary.ascuy@gmail.com>
2021-08-29 15:54:13 -0500

    Adding Ko-fi button

--- site/pages/_document.tsx
+++ site/pages/_document.tsx
@@ -19,10 +19,23 @@ export default class CertiCovidDocument extends Document {
             `,
             }}
           />
+
+          {/* Ko-fi Button */}
+          <script async type='text/javascript' src='https://storage.ko-fi.com/cdn/widget/Widget_2.js' />
         </Head>
         <body>
           <Main />
           <NextScript />
+
+          {/* Ko-fi Button */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              kofiwidget2.init("Buy Us a Coffee", "#29abe0", "E1E25YVX9");
+              kofiwidget2.draw();
+            `,
+            }}
+          />
         </body>
       </Html>
     )
